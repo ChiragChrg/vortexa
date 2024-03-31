b308f3766ce2af7d19dc3d66bdcd221261cb2b60
ChiragChrg <chiruchirag2001@gmail.com>
2024-03-31 23:39:56 +0530

    Created Custom Toast alert for Preact

--- src/components/Preact/SearchBar.tsx
+++ src/components/Preact/SearchBar.tsx
@@ -1,8 +1,7 @@
 import { useState, useEffect, useRef } from 'preact/hooks'
 import { FetchWeatherQuery, FetchWeatherPosition } from '../../hooks/useFetchWeather';
 import MapPin from '../SVG/MapPin';
-import { weather } from '../../store/weatherStore';
-import { toast } from 'react-hot-toast';
+import { toastData, weather } from '../../store/weatherStore';
 import { useIconCode } from '../../hooks/useIconCode';
 
 
@@ -41,13 +40,29 @@ const SearchBar = () => {
     const handleError = (error: any) => {
         console.log("Error:", error);
         switch (error.code) {
-            case error.PERMISSION_DENIED: toast.error(error.message)
+            case error.PERMISSION_DENIED:
+                toastData.set({
+                    status: "error",
+                    message: error.message
+                })
                 break;
-            case error.POSITION_UNAVAILABLE: toast.error(error.message)
+            case error.POSITION_UNAVAILABLE:
+                toastData.set({
+                    status: "error",
+                    message: error.message
+                })
                 break;
-            case error.TIMEOUT: toast.error(error.message)
+            case error.TIMEOUT:
+                toastData.set({
+                    status: "error",
+                    message: error.message
+                })
                 break;
-            default: toast.error(error.message)
+            default:
+                toastData.set({
+                    status: "error",
+                    message: error.message
+                })
         }
 
         // If geolocation access is denied, fetch results for "mangalore" by default
@@ -67,7 +82,10 @@ const SearchBar = () => {
                 { enableHighAccuracy: true, maximumAge: 10000 })
         } else {
             console.log("No GeoLocation Support")
-            toast.error("No GeoLocation Support")
+            toastData.set({
+                status: "error",
+                message: "No GeoLocation Support"
+            })
         }
     };
 
--- src/store/weatherStore.ts
+++ src/store/weatherStore.ts
@@ -6,3 +6,16 @@ export const weather = atom<weatherType | null>(null)
 export const imageCode = atom<string | null>(null)
 
 export const imperialUnit = atom<boolean>(false)
+
+type ToastType = {
+    status: "success" | "error" | "warn",
+    message: string,
+    autoClose?: number,
+    position?: "top-center",
+    toastID?: number,
+}
+
+export const toastData = atom<ToastType | null>({
+    status: "success",
+    message: "Tester bro"
+})
